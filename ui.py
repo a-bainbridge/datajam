ec0cccebb679524fa32f03a9cbf9f5066c6c3be8
malcolm <94253924+malcolm7633@users.noreply.github.com>
2023-01-29 02:53:58 -0600

    everything but glue

--- ui.py
+++ ui.py
@@ -1,14 +1,26 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout
 
+def ondaclick():
+    print(outbox.toPlainText())
+
+def recordevent():
+    inbox.setText('the decoded message')
+
 app = QApplication([])
 window = QWidget()
 layout = QHBoxLayout()
 layout1 = QVBoxLayout()
 layout2 = QVBoxLayout()
-layout1.addWidget(QTextEdit('Message to send'))
-layout2.addWidget(QTextEdit('Recorded message'))
-layout1.addWidget(QPushButton('Play'))
-layout2.addWidget(QPushButton('Record'))
+outbox = QTextEdit('Message to send')
+layout1.addWidget(outbox)
+inbox = QTextEdit('Recorded message')
+layout2.addWidget(inbox)
+playbutton = QPushButton('Play')
+playbutton.clicked.connect(ondaclick)
+layout1.addWidget(playbutton)
+recordbutton = QPushButton('Record')
+recordbutton.clicked.connect(recordevent)
+layout2.addWidget(recordbutton)
 layout.addLayout(layout1)
 layout.addLayout(layout2)
 window.setLayout(layout)
