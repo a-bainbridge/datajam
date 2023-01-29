21d2f2fbca03ca42e410ffbe275a7df6ecbd5c4e
malcolm <94253924+malcolm7633@users.noreply.github.com>
2023-01-29 08:20:55 -0600

    sine

--- ui.py
+++ ui.py
@@ -1,7 +1,15 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout
+from PyQt5 import QtCore
 import texttochords
 import record
 import playchords
+from matplotlib import pyplot as plt
+from matplotlib import figure
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+from matplotlib import use
+import numpy as np
+
+use('QtAgg')
 
 def playevent():
     playchords.play_chords(texttochords.encode(outbox.toPlainText()))
@@ -10,8 +18,18 @@ def recordevent():
     inbox.setText(record.listen())
     #inbox.setText(texttochords.decode(record.listolistonotes2indices(texttochords.encode(outbox.toPlainText()))))
 
+def update_plot():
+    global a
+    ax.cla()
+    ax.plot(np.sin(a)*np.sin(np.linspace(0,2*np.pi,100)))
+    ax.set_axis_off()
+    ax.set_ylim((-1,1),auto=False)
+    canvas.draw()
+    a+=.2
+
 app = QApplication([])
 window = QWidget()
+withwave = QVBoxLayout()
 layout = QHBoxLayout()
 layout1 = QVBoxLayout()
 layout2 = QVBoxLayout()
@@ -27,6 +45,19 @@ recordbutton.clicked.connect(recordevent)
 layout2.addWidget(recordbutton)
 layout.addLayout(layout1)
 layout.addLayout(layout2)
-window.setLayout(layout)
+withwave.addLayout(layout)
+fig=figure.Figure(facecolor=(.1,.1,.1))
+ax=fig.add_subplot()
+canvas=FigureCanvasQTAgg(fig)
+ax.plot(np.linspace(0,0,100))
+ax.set_axis_off()
+ax.set_ylim((-1,1),auto=False)
+withwave.addWidget(canvas)
+a=0
+window.timer=QtCore.QTimer()
+window.timer.setInterval(50)
+window.timer.timeout.connect(update_plot)
+window.timer.start()
+window.setLayout(withwave)
 window.show()
 app.exec()
